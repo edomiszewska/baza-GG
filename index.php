3e3d25a5f0588cc8351f4f6698f1c19f59b933b6
emiiiiiiiiiiiiiii <102521843+edomiszewska@users.noreply.github.com>
2024-11-28 12:09:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,46 @@
 $baza=mysqli_connect('localhost', 'root','','fryzjer');
 
 
+if(isset($_POST['pracownik'])){
+$imiep=$_POST['imiep'];
+$nazwiskop=$_POST['nazwiskop'];
+$zapytanie="insert into pracownicy(imie_p, nazwisko_p) values('$imiep','$nazwiskop');";
+mysqli_query($baza,$zapytanie);
 
+}
 
+if(isset($_POST['klient'])){
+    $imiek=$_POST['imiek'];
+    $nazwiskok=$_POST['nazwiskok'];
+    $pesel=$_POST['pesel'];
+    $zapytanie1="insert into klienci(imie, nazwisko, pesel) values('$imiek','$nazwiskok','$pesel');";
+    mysqli_query($baza,$zapytanie1);
+    }
 
+    if(isset($_POST['wizyta'])){
+        $nazwa=$_POST['wybierz'];
+        $data=$_POST['data'];
+        $cena=0;
+        
+        switch($nazwa)
+        {
+            case "farbowanie": $cena=170;
+            break;
 
+            case "rozjasnianie": $cena=200;
+            break;
+
+            case "strzyzenie": $cena=50;
+            break;
+
+            case "obciecie": $cena=80;
+            break;
+        } ;
+        $zapytanie2="insert into wizyty(nazwa, cena, data) values('$nazwa','$cena','$data');";
+        mysqli_query($baza,$zapytanie2);
+
+        }
+    
 
 ?>
 
@@ -27,16 +63,17 @@ $baza=mysqli_connect('localhost', 'root','','fryzjer');
 <form action="index.php"  method="POST">
 
 <label for="pracownicy">Dodaj pracownika:<br> 
-imię:<input type="text"><br> 
-nazwisko: <input type="text">
-
+imię:<input type="text" name='imiep'><br> 
+nazwisko: <input type="text" name='nazwiskop'>
+<br><button name='pracownik'>Dodaj</button>
 </label><br>
 
 <label for="klienci">Dodaj klienta<br> 
 
-imię:<input type="text"><br> 
-nazwisko:<input type="text"><br>
-pesel: <input type="number">
+imię:<input type="text" name='imiek'><br> 
+nazwisko:<input type="text" name='nazwiskok'><br>
+pesel: <input type="number" name='pesel'>
+<br><button name='klient'>Dodaj</button>
 </label><br>
 
 <label for="wizyta">Dodaj wizytę<br>
@@ -44,12 +81,13 @@ nazwa: <select name="wybierz" id="wybierz">
     <option value="farbowanie">farbowanie</option>
     <option value="rozjasnianie">rozjasnianie</option>
     <option value="strzyzenie">strzyzenie</option>
-    <option value="obciecie">obcięcie</option>
+    <option value="obciecie">obciecie</option>
 </select><br>
 
 <label for="data">Data: <br>
-    <input type="date"><br>
-    Godzina wizyty:<br><input type="time" name="" id="">
+    <input type="date" name='data'>   <br> 
+    
+    <button name='wizyta'>Prześlij</button> 
     
 </label>
 
@@ -62,6 +100,11 @@ nazwa: <select name="wybierz" id="wybierz">
 
 </body>
 </html>
+
+
+
+
+
 
 
 
